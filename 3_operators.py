f4eb61bd3ae4d8e1c001006e028529b5274deabd
Daniel Silva <dsemanuel15@gmail.com>
2025-02-10 17:14:41 -0300

    Python lessons

---
+++ 3_operators.py
@@ -0,0 +1,11 @@
+number = 8
+print('')
+
+# if number > 10:
+#    print('Right!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right!') if number > 10 else print('Not today')
+print('')
